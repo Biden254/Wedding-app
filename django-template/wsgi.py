1d26cb81362f3d2355cd915cbcf60c009c1fdcec
Biden254 <159169843+Biden254@users.noreply.github.com>
2025-08-28 19:09:18 +0300

    Fix django-temlate error

--- django-template/wsgi.py
+++ django-template/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wedding.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-template.settings')
 
 application = get_wsgi_application()
